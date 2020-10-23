2e410ad759a51385269e4dc558439cf97e2d9af9
seha13 <out_sherreraa@independencia.com.mx>
2020-10-23 10:18:40 -0500

    se creo carpeta para work y se agregaron dependencias al package-lock

---
+++ src/templates/work.js
@@ -0,0 +1,25 @@
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Layout from "../components/layout"
+ 
+// const WorkTemplate = ({ data }) => (
+//   <Layout>
+//     <h1>{data.strapiWork.title}</h1>
+//     <p>{data.strapiWork.description}</p>
+//     <p>{data.strapiWork.image}</p>
+//   </Layout>
+// )
+ 
+// export default WorkTemplate
+ 
+// export const query = graphql`
+//   query WorkTemplate($id: Int!) {
+//     strapiWork(strapiId: { eq: $id }) {
+//       title
+//       description
+//       image {
+          
+//       }
+//     }
+//   }
+// `
